290f561f4303bbd3a024da3ccc9bc02effab38a9
Juhwan Choi <jumax108@kakao.com>
2022-05-17 14:00:34 +0900

    20220516

--- profilerTLS/profilerTLS.cpp
+++ profilerTLS/profilerTLS.cpp
@@ -51,16 +51,22 @@ void CProfilerTLS::printToFile() {
 	fopen_s(&outFile, "profilerTLS.txt", "w");
 
 	fprintf_s(outFile, "%15s | %20s | %15s | %15s | %15s | %15s \n", "Thread ID", "Name", "Average", "Min", "Max", "Call");
+	
+	CProfiler::stProfile totalProfile[profiler::MAX_PROFILE_NUM];
 
 	for (; profilerIter != profilerEnd; ++profilerIter) {
 
 		CProfiler::stProfile* profileData = profilerIter->begin();
 
-		do {
+		for (; profileData != nullptr; profileData = profilerIter->next()) {
 
 			profileData->_sum = 
 				profileData->_sum - profileData->_max - profileData->_min;
 			profileData->_callCnt -= 2;
+
+			if (profileData->_callCnt <= 0) {
+				continue;
+			}
 
 			fprintf_s(outFile, "%15d | %20s | %12.3lf us | %12.3lf us | %12.3lf us | %15d \n",
 				profileData->_threadId,
@@ -70,12 +76,44 @@ void CProfilerTLS::printToFile() {
 				profileData->_max / (double)_freq.QuadPart,
 				profileData->_callCnt);
 
-			profileData = profilerIter->next();
+			for (int profileCnt = 0; profileCnt < profiler::MAX_PROFILE_NUM; ++profileCnt) {
 
-		} while (profileData != nullptr);
+				if (strcmp(profileData->_name, totalProfile[profileCnt]._name) == 0) {
+
+					totalProfile[profileCnt]._sum += profileData->_sum;
+					totalProfile[profileCnt]._callCnt += profileData->_callCnt;
+					break;
+				}
+
+				else if (totalProfile[profileCnt]._name[0] == '\0') {
+
+					strcpy_s(totalProfile[profileCnt]._name, profileData->_name);
+					totalProfile[profileCnt]._sum += profileData->_sum;
+					totalProfile[profileCnt]._callCnt += profileData->_callCnt;
+					break;
+				}
+			}
+
+			
+
+		}
+
+
 
 	}
 
+	fprintf_s(outFile, "%20s | %15s | %15s \n", "Name", "Average", "Call");
+	//totalProfile[profiler::MAX_PROFILE_NUM]
+	for (int profileCnt = 0; profileCnt < profiler::MAX_PROFILE_NUM; ++profileCnt) {
+		if (totalProfile[profileCnt]._callCnt <= 0) {
+			continue;
+		}
+
+		fprintf_s(outFile, "%20s | %12.3lf us | %15d \n",
+			totalProfile[profileCnt]._name,
+			totalProfile[profileCnt]._sum / (double)_freq.QuadPart / totalProfile[profileCnt]._callCnt,
+			totalProfile[profileCnt]._callCnt);
+	}
 }
 
 void CProfilerTLS::reset() {
